dbe4e593909ccc9e1e737ec15a0158cda3f41189
Benjamin Erhart <berhart@netzarchitekten.com>
2025-07-16 15:04:53 +0200

    Use curl instead of wget in download script, as wget is not installed by default on macOS.

--- AnyoneKit/download.sh
+++ AnyoneKit/download.sh
@@ -2,6 +2,10 @@
 
 VERSION=$1
 
+load() {
+    curl --remote-name --progress-bar --location $1
+}
+
 cd AnyoneKit/Assets
 
 # Test if file is older then 1 week.
@@ -9,8 +13,8 @@ OLD="$(find geoip -mmin +10080 2>/dev/null)"
 
 # Only download, if files are not existing or older than 1 week.
 if [ ! -f geoip -o ! -z "$OLD" ]; then
-    wget --output-document=geoip https://raw.githubusercontent.com/anyone-protocol/ator-protocol/refs/heads/main/src/config/geoip
-    wget --output-document=geoip6 https://raw.githubusercontent.com/anyone-protocol/ator-protocol/refs/heads/main/src/config/geoip6
+    load https://raw.githubusercontent.com/anyone-protocol/ator-protocol/refs/heads/main/src/config/geoip
+    load https://raw.githubusercontent.com/anyone-protocol/ator-protocol/refs/heads/main/src/config/geoip6
 fi
 
 cd ../..
@@ -19,7 +23,7 @@ cd ../..
 OLD="$(find anon.xcframework -mmin +10080 2>/dev/null)"
 
 if [ ! -d anon.xcframework -o ! -z "$OLD" ]; then
-    wget "https://github.com/anyone-protocol/AnyoneKit/releases/download/$VERSION/anon.xcframework.zip"
+    load "https://github.com/anyone-protocol/AnyoneKit/releases/download/$VERSION/anon.xcframework.zip"
     unzip anon.xcframework.zip
     rm anon.xcframework.zip
 fi
